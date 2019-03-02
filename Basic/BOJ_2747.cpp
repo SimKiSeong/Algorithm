e426fbaefb56e87dc775e2f072448db949b3c198
SimKiSeong <totokisung@naver.com>
2019-03-02 19:45:16 +0900

    solve : 2742 기찍 N

---
+++ Basic/BOJ_2747.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int a;
+    cin >> a;
+
+    for (int i = a; i > 0; i--){
+        cout << i;
+        printf("\n");
+    }
+
+    return 0;
+
+}
+
